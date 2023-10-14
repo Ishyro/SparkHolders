5f96c49c085141220a97050d19457726d952e5f2
Ishyro <julien.bissey@gmail.com>
2023-10-14 23:11:04 +0200

    fix fire effect resize

--- src/clients/graphics/godot_client_spark_holders/models/fire.gd
+++ src/clients/graphics/godot_client_spark_holders/models/fire.gd
@@ -3,15 +3,13 @@ extends Node3D
 func initialize(coords: Vector3, size: float, light_power: int):
 	transform.origin = coords
 	$Flames.lifetime *= size
+	$Flames.draw_pass_1 = QuadMesh.new()
 	$Flames.draw_pass_1.size = Vector2(size, size)
 	$Flames.process_material.emission_sphere_radius *= size 
 	$Smoke.lifetime *= size
+	$Smoke.draw_pass_1 = QuadMesh.new()
 	$Smoke.draw_pass_1.size = Vector2(size, size)
 	$Smoke.process_material.emission_sphere_radius *= size
-	$ParticlesFloating.amount *= size
-	$ParticlesFloating.lifetime *= size
-	$ParticlesFloating.draw_pass_1.size = Vector2(size, size)
-	$ParticlesFloating.process_material.emission_sphere_radius *= size
 	$Light.omni_range = light_power
 	$Light.light_energy *= light_power
 	#$Light.light_color = Color.WHITE
